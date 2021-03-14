5674ca6b1ca6ed3c46c260f999354bcc246e528a
Borbáth Eszter <61252275+eszter1999@users.noreply.github.com>
2021-03-14 15:24:29 +0100

    3.5

--- hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
@@ -1,9 +1,11 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
-// import hu.bme.mit.yakindu.analysis.RuntimeService;
-// import hu.bme.mit.yakindu.analysis.TimerService;
+import hu.bme.mit.yakindu.analysis.RuntimeService;
+import hu.bme.mit.yakindu.analysis.TimerService;
 import hu.bme.mit.yakindu.analysis.example.ExampleStatemachine;
 import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 
@@ -11,7 +13,7 @@ import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 
 public class RunStatechart {
 	
-	public static void main(String[] args) throws IOException {
+//	public static void main(String[] args) throws IOException {
 //		ExampleStatemachine s = new ExampleStatemachine();
 //		s.setTimer(new TimerService());
 //		RuntimeService.getInstance().registerStatemachine(s, 200);
@@ -26,10 +28,51 @@ public class RunStatechart {
 //		s.runCycle();
 //		print(s);
 //		System.exit(0);
-	}
-
+//	}
+//
 	public static void print(IExampleStatemachine s) {
 		System.out.println("W = " + s.getSCInterface().getWhiteTime());
 		System.out.println("B = " + s.getSCInterface().getBlackTime());
+	}
+	
+	public static void main(String[] args) throws IOException {
+		//making state machine
+		ExampleStatemachine s = new ExampleStatemachine();
+		s.setTimer(new TimerService());
+		RuntimeService.getInstance().registerStatemachine(s, 200);
+		s.init();
+		s.enter();
+		s.runCycle();
+		//reading inputs
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String esemeny;
+		while(true) {
+			esemeny = reader.readLine();
+			switch (esemeny) {
+			case "white":
+				s.raiseWhite();
+				s.runCycle();
+				print(s);
+				break;
+			case "black":
+				s.raiseBlack();
+				s.runCycle();
+				print(s);
+				break;
+			case "start":
+				s.raiseStart();
+				s.runCycle();
+				print(s);
+				break;
+			case "exit":
+				s.exit();
+				print(s);
+				System.exit(0);
+				break;
+			default:
+				System.out.println("ervenytelen");
+				break;
+			}
+		}
 	}
 }
